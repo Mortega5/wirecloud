b094f6ced4dbac73d082780b559c25f7ff0481e6
Miguel Ortega <miguel.ortega@seamware.com>
2026-01-19 11:54:42 +0100

    allow empty roles

--- src/wirecloud/backends/vc_backend.py
+++ src/wirecloud/backends/vc_backend.py
@@ -94,4 +94,6 @@ class VCBackend(ModelBackend):
 
     def _get_roles(self, vc_payload: VCPayload):
         client_id = settings.VC_LOGIN_CONFIG['role_target']
+        if not vc_payload.roles:
+            return []
         return next((role['names'] for role in vc_payload.roles if role['target'] == client_id), [])
